eb7ada1fd3b7e0e1b8157119f547c27e9a14361d
Bernardino Sassoli <sassoli@gmail.com>
2022-05-20 16:57:44 +0200

    Modifies signin form in frontend to give user feedback

--- boombust/settings.py
+++ boombust/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,16 +88,29 @@ WSGI_APPLICATION = "boombust.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        "NAME": "users",
-        "USER": "admin",
-        "PASSWORD": "password",
-        "HOST": "localhost",
+
+if os.getenv('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'github-actions',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': 'localhost',
+            'PORT': '5432'
+        }
     }
-}
+else:
+        DATABASES = {
+            "default": {
+                "ENGINE": "django.db.backends.postgresql_psycopg2",
+                # 'NAME': BASE_DIR / 'db.sqlite3',
+                "NAME": "users",
+                "USER": "admin",
+                "PASSWORD": "password",
+                "HOST": "localhost",
+            }
+        }
 
 
 # Password validation
